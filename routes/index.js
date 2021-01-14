2e62016c6b24b46da208a70a1d2b4908e594fc15
J.Tadebois <jpm.tadebois@gmail.com>
2021-01-14 12:45:06 +0400

    Add express controller and router for Student model

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const classroomController = require('../controllers').classroom;
+const studentController = require('../controllers').student;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -12,5 +13,11 @@ router.get('/api/classroom/:id', classroomController.getById);
 router.post('/api/classroom', classroomController.add);
 router.put('/api/classroom/:id', classroomController.update);
 router.delete('/api/classroom/:id', classroomController.delete);
+
+router.get('/api/student', studentController.list);
+router.get('/api/student/:id', studentController.getById);
+router.post('/api/student', studentController.add);
+router.put('/api/student/:id', studentController.update);
+router.delete('/api/student/:id', studentController.delete);
 
 module.exports = router;
